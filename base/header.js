13bf2a409a69871b5c2e1cb2c51a6bc4722f0842
RamonAlvesFerreira <ramon115dangerline@gmail.com>
2024-05-18 12:23:55 -0300

    Ajustes no Header

--- base/header.js
+++ base/header.js
@@ -28,6 +28,11 @@ function createHeader(headers) {
         var a_companyButton = document.createElement('a');
         var a_newsButton = document.createElement('a');
 
+        var p_homeText = document.createElement('p');
+        var p_newsText = document.createElement('p');
+        var p_gamesText = document.createElement('p');
+        var p_companyText = document.createElement('p');
+
         /*INFOS */
 
         nav.classList.add('HeaderBase');
@@ -41,9 +46,10 @@ function createHeader(headers) {
         img_logo.src = 'https://i.imgur.com/i0lSBcs.png';
 
         a_logo.classList.add('component');
-        a_logo.classList.add('image-link');
         a_logo.classList.add('col-lg-2');
         a_logo.href = 'index.html';
+
+
 
         a_logo.appendChild(img_logo);
         divRow.appendChild(a_logo);
@@ -56,62 +62,74 @@ function createHeader(headers) {
         divRow.appendChild(a_buttons);
 
         //Home
-        a_homeButton.classList.add('MyButtons');
+        a_homeButton.classList.add('FirstHeaderButtons');
         a_homeButton.classList.add('btn');
         a_homeButton.classList.add('btn-default');
         a_homeButton.classList.add('col-xs-8');
         a_homeButton.classList.add('col-sm-6');
         a_homeButton.classList.add('col-md-2');
         a_homeButton.classList.add('col-lg-1');
-        a_homeButton.classList.add('HeaderButtons');
         a_homeButton.href = 'index.html';
         a_homeButton.role = 'button';
-        a_homeButton.innerHTML = 'Home';
+        //a_homeButton.innerHTML = 'Home';
+
+        p_homeText.classList.add('TextHeaderButtons');
+        p_homeText.innerHTML = 'Home';
+        a_homeButton.appendChild(p_homeText);
 
         divRow.appendChild(a_homeButton);
 
         //News
-        a_newsButton.classList.add('MyButtons');
+        a_newsButton.classList.add('HeaderButtons');
         a_newsButton.classList.add('btn');
         a_newsButton.classList.add('btn-default');
         a_newsButton.classList.add('col-xs-10');
         a_newsButton.classList.add('col-sm-6');
         a_newsButton.classList.add('col-md-2');
         a_newsButton.classList.add('col-lg-1');
-        a_newsButton.classList.add('HeaderButtons');
         a_newsButton.href = 'News.html';
         a_newsButton.role = 'button';
-        a_newsButton.innerHTML = 'News';
+        //a_newsButton.innerHTML = 'News';
+
+        p_newsText.classList.add('TextHeaderButtons');
+        p_newsText.innerHTML = 'News';
+        a_newsButton.appendChild(p_newsText);
 
         divRow.appendChild(a_newsButton);
 
         //Games
-        a_gamesButton.classList.add('MyButtons');
+        a_gamesButton.classList.add('HeaderButtons');
         a_gamesButton.classList.add('btn');
         a_gamesButton.classList.add('btn-default');
         a_gamesButton.classList.add('col-xs-10');
         a_gamesButton.classList.add('col-sm-6');
         a_gamesButton.classList.add('col-md-2');
         a_gamesButton.classList.add('col-lg-1');
-        a_gamesButton.classList.add('HeaderButtons');
         a_gamesButton.href = 'Games.html';
         a_gamesButton.role = 'button';
-        a_gamesButton.innerHTML = 'Games';
+        //a_gamesButton.innerHTML = 'Games';
+
+        p_gamesText.classList.add('TextHeaderButtons');
+        p_gamesText.innerHTML = 'Games';
+        a_gamesButton.appendChild(p_gamesText);
 
         divRow.appendChild(a_gamesButton);
 
         //Company
-        a_companyButton.classList.add('MyButtons');
+        a_companyButton.classList.add('HeaderButtons');
         a_companyButton.classList.add('btn');
         a_companyButton.classList.add('btn-default');
         a_companyButton.classList.add('col-xs-10');
         a_companyButton.classList.add('col-sm-6');
         a_companyButton.classList.add('col-md-2');
         a_companyButton.classList.add('col-lg-1');
-        a_companyButton.classList.add('HeaderButtons');
         a_companyButton.href = 'Company.html';
         a_companyButton.role = 'button';
-        a_companyButton.innerHTML = 'Company';
+        //a_companyButton.innerHTML = 'Company';
+
+        p_companyText.classList.add('TextHeaderButtons');
+        p_companyText.innerHTML = 'Company';
+        a_companyButton.appendChild(p_companyText);
 
         divRow.appendChild(a_companyButton);
 
